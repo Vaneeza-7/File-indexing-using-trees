b89f429f280e4b77d687d0bf4908cc0b20e9a893
Vaneeza Ahmad <vaneezaahmad786@gmail.com>
2022-11-24 09:34:16 +0500

    Add project files.

---
+++ DataStructProject/rbTree.h
@@ -0,0 +1,28 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class rbNode {
+    T value;
+    rbNode* left;
+    rbNode* right;
+    int colour;    //0 represents black & 1 represents red
+public:
+    rbNode()
+    {
+        T value;
+        left = NULL;
+        right = NULL;
+        colour = 1;
+    }
+    void setValue()
+    {
+
+    }
+
+    void setColour()
+    {
+
+    }
+
+
+};
